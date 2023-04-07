1bfcaf9da4bc71d3909a86f021c5c09abf1afe4a
Roshit Raj <roshitraj@Roshits-MacBook-Air.local>
2023-04-07 11:36:17 +0530

    made pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,40 @@
-import { defineConfig } from 'vite';
-import {VitePWA} from 'vite-plugin-pwa'
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
+import solidPlugin from "vite-plugin-solid"
 
 export default defineConfig({
-  plugins: [solidPlugin(),VitePWA({injectRegister: "script"})],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+    plugins: [
+        solidPlugin(),
+        VitePWA({
+            manifest: {
+                id: "splitwise",
+                name: "Splitwise",
+                short_name: "Splitwise",
+                start_url: "/index.html",
+                scope: "/",
+                icons: [
+                    {
+                        src: "/android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                display: "standalone",
+            },
+            strategies: "generateSW",
+        }),
+    ],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: "esnext",
+    },
+})
